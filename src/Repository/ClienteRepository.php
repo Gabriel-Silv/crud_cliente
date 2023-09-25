2fa210da06b006084696c066c7957891ba75a1cc
Gabriel-Silv <gmswbb.2silva@gmail.com>
2023-09-25 18:05:37 -0300

    crud clientes

--- src/Repository/ClienteRepository.php
+++ src/Repository/ClienteRepository.php
@@ -3,7 +3,6 @@ namespace app\Repository;
 
 //require './vendor/autoload.php';
 use app\Model\Cliente;
-use Exception;
 use PDO;
 use PDOException;
 
@@ -17,7 +16,20 @@ class ClienteRepository
         $this->conn = $conn;
     }
 
+   
+   
     public function findById($id)
+    {
+        $query = " SELECT clientes.* FROM clientes ";
+        $query = $query . "  WHERE clientes.id = :id ";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function findByClienteIdWithEndereco($id)
     {
         $query = " SELECT clientes.*, enderecos.* FROM clientes ";
         $query = $query . " inner join enderecos on clientes.id = enderecos.id_cliente";
@@ -36,34 +48,37 @@ class ClienteRepository
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     }
-
     public function create(Cliente $cliente)
     {
-        
-      try{  
-        
-        $query = "INSERT INTO clientes (razao_social, nome_fantasia, email, telefone, cnpj)
-                  VALUES (:razao_social, :nome_santasia, :email, :telefone, :cnpj)";
-        $stmt = $this->conn->prepare($query);
-        $razao = $cliente->getRazaoSocial();
-        $fantasia = $cliente->getNomeFantasia();
-        $email = $cliente->getEmail();
-        $telefone = $cliente->getTelefone();
-        $cnpj = $cliente->getCnpj();
 
-        $stmt->bindParam(":razao_social", $razao);
-        $stmt->bindParam(":nome_santasia", $fantasia);
-        $stmt->bindParam(":email", $email);
-        $stmt->bindParam(":telefone", $telefone);
-        $stmt->bindParam(":cnpj", $cnpj);
-        //$stmt->bindParam(":endereco", $cliente->getEndereco());
-        $stmt->execute();
-        $id = $this->conn->lastInsertId();
-        $result = $this->findById($id);
-       
-        return $result ;
+        try {
+            $query = "INSERT INTO clientes (razao_social, nome_fantasia, email, telefone, cnpj)
+                  VALUES (:razao_social, :nome_santasia, :email, :telefone, :cnpj)";
+            $stmt = $this->conn->prepare($query);
+            $razao = $cliente->getRazaoSocial();
+            $fantasia = $cliente->getNomeFantasia();
+            $email = $cliente->getEmail();
+            $telefone = $cliente->getTelefone();
+            $cnpj = $cliente->getCnpj();
+
+            $this->conn->beginTransaction();
+
+            $stmt->bindParam(":razao_social", $razao);
+            $stmt->bindParam(":nome_santasia", $fantasia);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":telefone", $telefone);
+            $stmt->bindParam(":cnpj", $cnpj);
+            //$stmt->bindParam(":endereco", $cliente->getEndereco());
+            $stmt->execute();
+            $id = $this->conn->lastInsertId();
+         
+            $this->conn->commit();
+            $result = $this->findById($id);
+            return $this->returnCliente($result[0],$cliente);
         } catch (PDOException $e) {
+            $this->conn->rollBack();
             die($e->getMessage());
+
         }
     }
 
@@ -79,14 +94,14 @@ class ClienteRepository
             $email = $cliente->getEmail();
             $telefone = $cliente->getTelefone();
             $cnpj = $cliente->getCnpj();
-            $id=$cliente->getId();
+            $id = $cliente->getId();
             $stmt->bindParam(":razao_social", $razao);
             $stmt->bindParam(":nome_fantasia", $fantasia);
             $stmt->bindParam(":email", $email);
             $stmt->bindParam(":telefone", $telefone);
             $stmt->bindParam(":cnpj", $cnpj);
             //$stmt->bindParam(":endereco", $cliente->getEndereco());
-            $stmt->bindParam(":id",$id);
+            $stmt->bindParam(":id", $id);
             // endereço
             return $stmt->execute();
         } catch (PDOException $e) {
@@ -116,7 +131,7 @@ class ClienteRepository
 
     }
 
-    public function returnCliente($result, $cliente)
+    public function returnCliente($result,Cliente $cliente)
     {
         $cliente->setId($result['id']);
         $cliente->setRazaoSocial($result['razao_social']);
@@ -124,7 +139,9 @@ class ClienteRepository
         $cliente->setEmail($result['email']);
         $cliente->setTelefone($result['telefone']);
         $cliente->setCnpj($result['cnpj']);
-        //$cliente->setEndereco($result['endereco']);
+        if(isset($result['endereco'])){
+            $cliente->setEndereco($result['endereco']);
+        }
         return $cliente;
     }
 
